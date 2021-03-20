ea0af3cce8a1cab0d9715be63e7760795422286f
Alexey Dudarev <dudleks@gmail.com>
2021-03-20 22:15:31 +0300

    Java. Уровень 3. Задание 1. Решение практического задания

--- src/com/gb/adudarev/level3/MainClassLevel3Lesson1.java
+++ src/com/gb/adudarev/level3/MainClassLevel3Lesson1.java
@@ -3,5 +3,7 @@ package com.gb.adudarev.level3;
 public class MainClassLevel3Lesson1 {
     public static void main(String[] args) {
         Level3Lesson1Item1.start();
+        System.out.println();
+        Level3Lesson1Item2.start();
     }
 }
