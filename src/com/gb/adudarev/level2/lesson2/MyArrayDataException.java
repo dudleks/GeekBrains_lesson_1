2a6b9a755ca6d2e26e664acd5c1441d6170f33c4
Alexey Dudarev <dudleks@gmail.com>
2021-03-21 10:44:59 +0300

    Рефакторинг проектной структуры и иерархии #2

---
+++ src/com/gb/adudarev/level2/lesson2/MyArrayDataException.java
@@ -0,0 +1,8 @@
+package com.gb.adudarev.level2.lesson2;
+
+public class MyArrayDataException extends NumberFormatException {
+
+    public MyArrayDataException(String s) {
+        super(s);
+    }
+}
